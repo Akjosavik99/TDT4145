493cd6c6d30b1b85d417a6e18cf22623663f4961
Leander Sebastian Thorstad <leandert@stud.ntnu.no>
2023-03-22 16:05:21 +0100

    la til en billett til ordre id 1

--- brukerhistoreH.sql
+++ brukerhistoreH.sql
@@ -6,7 +6,7 @@ INSERT INTO Kunde (navn, epost, tlf)
 INSERT INTO Kunde (navn, epost, tlf) 
   VALUES ("Andreas", "andreas@epost.no", "22222222");
 
--- Legger inn ordre
+-- Legger inn ordre (datoen her er bestillingsdato, ikke reisedato)
 INSERT INTO Ordre (dato, kundenummer) 
   VALUES ("2023-03-22", 1);
 INSERT INTO Ordre (dato, kundenummer) 
@@ -15,15 +15,35 @@ INSERT INTO Ordre (dato, kundenummer)
   VALUES ("2023-01-01", 2);
 
 -- Legger inn sittebilletter til Leander sin reise fra Trondheim S til Mosjoeen
-INSERT INTO Billett (forekomstID, ordrenummer)
-  VALUES (1, 1);
+-- Det er en billett per delstrekning
+-- Billett for reisende 1 av 2
+INSERT INTO Billett (billettID, forekomstID, ordrenummer)
+  VALUES (1, 1, 1);
 INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
   VALUES (1, 1, 1, 1, 1);
-INSERT INTO Billett (forekomstID, ordrenummer)
-  VALUES (1, 1);
+
+INSERT INTO Billett (billettID, forekomstID, ordrenummer)
+  VALUES (2, 1, 1);
 INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
   VALUES (2, 2, 1, 1, 1);
-INSERT INTO Billett (forekomstID, ordrenummer)
-  VALUES (1, 1);
+
+INSERT INTO Billett (billettID, forekomstID, ordrenummer)
+  VALUES (3, 1, 1);
 INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
   VALUES (3, 3, 1, 1, 1);
+
+-- Billett for reisende 2 av 2
+INSERT INTO Billett (billettID, forekomstID, ordrenummer)
+  VALUES (4, 1, 1);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (4, 1, 1, 1, 2);
+
+INSERT INTO Billett (billettID, forekomstID, ordrenummer)
+  VALUES (5, 1, 1);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (5, 2, 1, 1, 2);
+
+INSERT INTO Billett (billettID, forekomstID, ordrenummer)
+  VALUES (6, 1, 1);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (6, 3, 1, 1, 2);
