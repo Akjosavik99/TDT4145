fc5726624692c55a3c868b38e7887e48a5f68740
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-03-23 13:38:11 +0100

    flere billetter

--- brukerhistoreH.sql
+++ brukerhistoreH.sql
@@ -12,6 +12,8 @@ INSERT INTO Ordre (dato, kundenummer)
 INSERT INTO Ordre (dato, kundenummer) 
   VALUES ("2023-03-05", 1);
 INSERT INTO Ordre (dato, kundenummer) 
+  VALUES ("2023-03-06", 1);
+INSERT INTO Ordre (dato, kundenummer) 
   VALUES ("2023-01-01", 2);
 
 -- Legger inn sittebilletter til Leander sin reise fra Trondheim S til Mosjoeen
@@ -27,3 +29,31 @@ INSERT INTO Billett (forekomstID, ordrenummer)
   VALUES (1, 1);
 INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
   VALUES (3, 3, 1, 1, 1);
+
+  -- Legger inn sittebilletter til Leander sin reise fra Steinkjer til Fauske
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (2, 2);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (4, 2, 1, 1, 2);
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (2, 2);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (5, 3, 1, 1, 2);
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (2, 2);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (6, 4, 1, 1, 2);
+
+  -- Legger inn sittebilletter til Leander sin reise fra Mo i Rana til Trondheim S
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (5, 3);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (7, 3, 1, 1, 3);
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (5, 3);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (8, 2, 1, 1, 3);
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (5, 3);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (9, 1, 1, 1, 3);
