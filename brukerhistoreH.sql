61e5d1265dd10b3e015a15c687bcafcfd859c910
Leander Sebastian Thorstad <leandert@stud.ntnu.no>
2023-03-23 17:58:50 +0100

    Revert "Revert "Merge branch 'main' of gitlab.stud.idi.ntnu.no:andrkjos/database-prosjekt""
    This reverts commit 8922e156f07a038af8616452930a2b6f4927ec16

--- brukerhistoreH.sql
+++ brukerhistoreH.sql
@@ -1,5 +1,3 @@
---Sletter alle records i relevante tabller TODO
-
 -- Legger inn kunder
 INSERT INTO Kunde (navn, epost, tlf) 
   VALUES ("Leander", "leander@epost.com", "11111111");
@@ -11,6 +9,10 @@ INSERT INTO Ordre (dato, kundenummer)
   VALUES ("2023-03-22", 1);
 INSERT INTO Ordre (dato, kundenummer) 
   VALUES ("2023-03-05", 1);
+INSERT INTO Ordre (dato, kundenummer) 
+  VALUES ("2023-03-06", 1);
+INSERT INTO Ordre (dato, kundenummer) 
+  VALUES ("2023-03-07", 1);
 INSERT INTO Ordre (dato, kundenummer) 
   VALUES ("2023-01-01", 2);
 
@@ -46,4 +48,38 @@ INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenumm
 INSERT INTO Billett (billettID, forekomstID, ordrenummer)
   VALUES (6, 1, 1);
 INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
-  VALUES (6, 3, 1, 1, 2);
+  VALUES (3, 3, 1, 1, 1);
+
+  -- Legger inn sittebilletter til Leander sin reise fra Steinkjer til Fauske
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (2, 2);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (4, 2, 1, 1, 2);
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (2, 2);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (5, 3, 1, 1, 2);
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (2, 2);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (6, 4, 1, 1, 2);
+
+  -- Legger inn sittebilletter til Leander sin reise fra Mo i Rana til Trondheim S
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (5, 3);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (7, 3, 1, 1, 3);
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (5, 3);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (8, 2, 1, 1, 3);
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (5, 3);
+INSERT INTO Sittebillett (billettID, delstrekningID, vognID, radnummer, setenummer)
+  VALUES (9, 1, 1, 1, 3);
+
+  -- Legger inn sittebilletter til Andreas sin reise fra Trondheim S til Steinkjer
+INSERT INTO Billett (forekomstID, ordrenummer)
+  VALUES (4, 4);
+INSERT INTO Sovebillett (billettID, delstrekningID, antallSenger, kupenummer, vognID)
+  VALUES (10, 1, 2, 1, 3);
